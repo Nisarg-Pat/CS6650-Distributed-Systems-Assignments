392efbe2b2a6e0cc45b77d0919efef74562885ce
Nisarg Patel <patel321nisarg@gmail.com>
2022-09-29 19:32:48 -0400

    Infinitely running server

--- Project01/src/server/TCPServer.java
+++ Project01/src/server/TCPServer.java
@@ -11,29 +11,33 @@ import java.net.Socket;
 public class TCPServer {
 
   int port;
-  Socket socket;
+  ServerSocket serverSocket;
 
   public TCPServer(int port) throws IOException {
     this.port = port;
-    ServerSocket serverSocket = new ServerSocket(port);
-    socket = serverSocket.accept();
+    serverSocket = new ServerSocket(port);
+
   }
 
   public void execute() throws IOException {
-    InputStream socketIn = socket.getInputStream();
-    DataInputStream dataIn = new DataInputStream(socketIn);
-    String input = new String(dataIn.readUTF());
-    System.out.println("\nServer Input: " + input);
+    System.out.println("TCPServer started at port: " + port);
+    while(true) {
+      Socket socket = serverSocket.accept();
+      InputStream socketIn = socket.getInputStream();
+      DataInputStream dataIn = new DataInputStream(socketIn);
+      String input = new String(dataIn.readUTF());
+      System.out.println("\nServer Input: " + input);
 
-    //Sending the changed string
-    OutputStream socketOut = socket.getOutputStream();
-    DataOutputStream dataOut = new DataOutputStream(socketOut);
-    dataOut.writeUTF(input);
+      //Sending the changed string
+      OutputStream socketOut = socket.getOutputStream();
+      DataOutputStream dataOut = new DataOutputStream(socketOut);
+      dataOut.writeUTF(input);
 
-    dataIn.close();
-    socketIn.close();
-    dataOut.close();
-    socketOut.close();
-    socket.close();
+      dataIn.close();
+      socketIn.close();
+      dataOut.close();
+      socketOut.close();
+    }
+    //socket.close();
   }
 }
