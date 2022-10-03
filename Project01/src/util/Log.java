a7909b44d6b7468345db21527ab5ab99de5a4280
Nisarg Patel <patel321nisarg@gmail.com>
2022-10-03 13:28:10 -0400

    Removed throws IOException in Log

--- Project01/src/util/Log.java
+++ Project01/src/util/Log.java
@@ -8,34 +8,50 @@ public class Log {
 
   File file;
 
-  public Log() throws IOException {
+  public Log() {
     file = null;
   }
 
-  public void createFile(String s) throws IOException {
+  public void createFile(String s) {
     file = new File(s);
     logOnly("\n\n-------------------------------------------------------------------");
   }
 
-  public void log(String s) throws IOException {
+  public void log(String s){
     s = DataUtils.getCurrentTime()+": "+s;
-    FileWriter writer = new FileWriter(file, true);
-    System.out.print(s);
-    writer.write(s);
-    writer.close();
+    try {
+      FileWriter writer = new FileWriter(file, true);
+      System.out.print(s);
+      writer.write(s);
+      writer.close();
+    } catch (IOException e) {
+      System.out.println("Could not write on file "+file.getPath());
+    }
+
   }
 
-  public void logln(String s) throws IOException {
+  public void logln(String s) {
     s = DataUtils.getCurrentTime()+": "+s;
-    FileWriter writer = new FileWriter(file, true);
-    System.out.println(s);
-    writer.write(s+"\n");
-    writer.close();
+    FileWriter writer = null;
+    try {
+      writer = new FileWriter(file, true);
+      System.out.println(s);
+      writer.write(s+"\n");
+      writer.close();
+    } catch (IOException e) {
+      System.out.println("Could not write on file "+file.getPath());
+    }
+
   }
 
-  public void logOnly(String s) throws IOException {
-    FileWriter writer = new FileWriter(file, true);
-    writer.write(s+"\n");
-    writer.close();
+  public void logOnly(String s) {
+    FileWriter writer = null;
+    try {
+      writer = new FileWriter(file, true);
+      writer.write(s+"\n");
+      writer.close();
+    } catch (IOException e) {
+      System.out.println("Could not write on file "+file.getPath());
+    }
   }
 }
