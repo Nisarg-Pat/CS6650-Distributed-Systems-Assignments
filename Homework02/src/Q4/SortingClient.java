fd230098dac5cd4fbd1429819851453950c4fb15
Nisarg Patel <patel321nisarg@gmail.com>
2022-10-11 13:54:13 -0400

    Changes in Q4 Client, making it CLI

--- Homework02/src/Q4/SortingClient.java
+++ Homework02/src/Q4/SortingClient.java
@@ -6,12 +6,24 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class SortingClient {
+
+  public static Sorter sorter;
+
   public static void main(String[] args) {
     try {
-      Sorter sorter = (Sorter) Naming.lookup("rmi://localhost:1099/SortingService");
-      int[] arr = new int[]{3, 5, 8, 6, 7, 9, 4, 2, 1};
+      sorter = (Sorter) Naming.lookup("rmi://localhost:1099/SortingService");
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number of integers: ");
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      System.out.println("Enter "+n+" Integers(Format: a0 a1 a2 .. an): ");
+      for(int i = 0;i<n;i++) {
+        arr[i] = sc.nextInt();
+      }
+      System.out.println("Sorted Order: ");
       printArray(sorter.sort(arr));
     } catch (RemoteException re) {
       System.out.println(
